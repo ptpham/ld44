0ab6cb4e27eb5ef363d026a967c55b49185c7437
Cindy Zhang <cixzhang@gmail.com>
2019-04-27 10:25:27 -0700

    adding get and drop item states, describing the input object

--- js/fighter.js
+++ js/fighter.js
@@ -79,6 +79,36 @@ class FighterHitstun extends FighterState {
   }
 }
 
+class FighterGetItem extends FighterState {
+  constructor(fighter, item) {
+    super(fighter);
+    _.push(figher.items, item);
+    this.done = false;
+    setTimeout(() => { this.done = true; }, Math.max(item.cost, 1) * 1000);
+  }
+
+  update(input) {
+    let defaultState = this._updateDefault(input);
+    if (defaultState) return defaultState;
+    if (this.done) return new FighterStanding(this.fighter);
+  }
+}
+
+class FighterDropItem extends FighterState {
+  constructor(fighter, item) {
+    super(fighter);
+    _.pull(fighter.items, item);
+    this.done = false;
+    setTimeout(() => { this.done = true; }, 500);
+  }
+
+  update(input) {
+    let defaultState = this._updateDefault(input);
+    if (defaultState) return defaultState;
+    if (this.done) return new FighterStanding(this.fighter);
+  }
+}
+
 class Fighter {
   constructor(sprite) {
     this.items = [];
@@ -89,6 +119,13 @@ class Fighter {
   }
 
   update(input) {
+    // Input is anobject containing the following:
+    // * move: {x, y}
+    // * damage: number (amount of health taken)
+    // * hitstun: number (duration of the stun)
+    // * attacking: number (duration of the attack)
+    // * pickItem: item (item the player is picking up)
+    // * dropItem: item (item the player is dropping)
     this.state = this.state.update(input) || this.state;
   }
 }
--- js/state.js
+++ js/state.js
@@ -4,5 +4,11 @@ let state = {
   debugText: null,
   player: null,
   cursors: null,
+  allItems: [
+    {
+      name: 'Item 1',
+      cost: 1,
+    },
+  ],
 };
 
